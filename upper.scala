b009d2418a2426581f460cff40b7b55fbb47764f
Rahul Kumar <rahulsinner@gmail.com>
2011-06-21 22:20:28 +0530

    Re-factoring the folder structure.

--- upper.scala
+++
@@ -1,8 +0,0 @@
-class Upper {
-  def Upper(strings: String*): Seq[String] = {
-    strings.map((s:String) => s.toUpperCase())
-  }
-}
-
-val up = new Upper
-Console.println(up.Upper("A", "First", "Scala", "Program"))
--- upper3.scala
+++
@@ -1,6 +0,0 @@
-object Upper {
-  def main(args: Array[String]) = {
-    args.map(_.toUpperCase()).foreach(printf("%s ", _))
-    println("")
-  }
-}
